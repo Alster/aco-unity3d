0bb679fece8481b909ba92f19714ff76681b0dde
Alster <anik123wb@gmail.com>
2016-03-16 12:11:20 +0200

    adding eimt ignore *.meta

---
+++ Util/EIMT/ExecuterJ.cs
@@ -0,0 +1,10 @@
+﻿namespace ACO.EIMT
+{
+    public class ExecuterJ : EIMT<JSONObject>
+    {
+        public static void Add(System.Action<JSONObject> act, JSONObject msg)
+        {
+            AddTask(act, msg);
+        }
+    }
+}
